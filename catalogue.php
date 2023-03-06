1e010797eeecbd2c5f334f487c6b1d259abb1221
Mbesse6 <127052839+Mbesse6@users.noreply.github.com>
2023-03-06 09:29:35 +0100

    Add files via upload

---
+++ catalogue.php
@@ -0,0 +1,117 @@
+ <!DOCTYPE html>
+    <html>
+    <head>
+        <title>LIBRAIRMBESS</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/Style.css" rel="stylesheet" />
+        <link href="css/Style1.css" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+      </head>
+      <body>
+        <nav class="navbar">
+          <ul class="navbar-menu">
+            <li class="navbar-item logo">
+              <a href="#" class="navbar-link">
+                <span class="navbar-title">LIBRAIRIE</span>
+                <i class="fas fa-chevron-right navbar-icon"></i>
+              </a>
+            </li>
+            <li class="navbar-item">
+              <a href="#" class="navbar-link">
+                <i class="fas fa-home navbar-icon"></i>
+                <span class="navbar-title">Home</span>
+              </a>
+            </li>
+            <li class="navbar-item">
+              <a href="catalogue.php" class="navbar-link">
+                <i class="fas fa-folder navbar-icon"></i>
+                <span class="navbar-title">Folders</span>
+              </a>
+            </li>
+            <li class="navbar-item">
+              <a href="#" class="navbar-link">
+                <i class="fas fa-bell navbar-icon"></i>
+                <span class="navbar-title">Notifications</span>
+              </a>
+            </li>
+            <li class="navbar-item">
+              <a href="#" class="navbar-link">
+                <i class="fas fa-user navbar-icon"></i>
+                <span class="navbar-title">Reservations</span>
+              </a>
+            </li>
+            <li class="navbar-item">
+              <a href="#" class="navbar-link">
+                <i class="fas fa-phone navbar-icon"></i>
+                <span class="navbar-title">Contact</span>
+              </a>
+            </li>
+            <li class="navbar-item">
+              <a href="index.php" class="navbar-link">
+                <i class="fas fa-sign-out-alt navbar-icon"></i>
+                <span class="navbar-title">Exit</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+<div class="C">
+  <img src="image/rp.jpg" width="200px" height="160">
+  <a href="#"><img class="ij" src="image/images.jpeg" width="80" height="50" ></a>
+  <p class="p1">Profession : Medecin</p>
+</div>
+<div class="C">
+  <img src="image/rp.jpg" width="200px" height="160">
+  <a href="#"><img class="ij" src="image/images.jpeg" width="80" height="50" ></a>
+  <p class="p1">Profession : Medecin</p>
+</div>
+<div class="C">
+  <img src="image/rp.jpg" width="200px" height="160">
+  <a href="#"><img class="ij" src="image/images.jpeg" width="80" height="50" ></a>
+  <p class="p1">Profession : Medecin</p>
+</div>
+<div class="C">
+  <img src="image/rp.jpg" width="200px" height="160">
+  <a href="#"><img class="ij" src="image/images.jpeg" width="80" height="50" ></a>
+  <p class="p1">Profession : Medecin</p>
+</div>
+<div class="C">
+  <img src="image/rp.jpg" width="200px" height="160">
+  <a href="#"><img class="ij" src="image/images.jpeg" width="80" height="50" ></a>
+  <p class="p1">Profession : Medecin</p>
+</div>
+<div class="C">
+  <img src="image/rp.jpg" width="200px" height="160">
+  <a href="#"><img class="ij" src="image/images.jpeg" width="80" height="50" ></a>
+  <p class="p1">Profession : Medecin</p>
+</div>
+<div class="C">
+  <img src="image/rp.jpg" width="200px" height="160">
+  <a href="#"><img class="ij" src="image/images.jpeg" width="80" height="50" ></a>
+  <p class="p1">Profession : Medecin</p>
+</div>
+<div class="C">
+  <img src="image/rp.jpg" width="200px" height="160">
+  <a href="#"><img class="ij" src="image/images.jpeg" width="80" height="50" ></a>
+  <p class="p1">Profession : Medecin</p>
+</div>
+<div class="C">
+  <img src="image/rp.jpg" width="200px" height="160">
+  <a href="#"><img class="ij" src="image/images.jpeg" width="80" height="50" ></a>
+  <p class="p1">Profession : Medecin</p>
+</div>
+<div class="C">
+  <img src="image/rp.jpg" width="200px" height="160">
+  <a href="#"><img class="ij" src="image/images.jpeg" width="80" height="50" ></a>
+  <p class="p1">Profession : Medecin</p>
+</div>
+
+         
+             </form>
+         </div>
+         
+
+        </div>
+        </main>
+      </body>
+    </html>
